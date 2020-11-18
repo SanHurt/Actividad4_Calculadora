82014afba4964685167d7e5bf5caffb8e4473af1
Sandra Hurtado <sandra.hurtado16@iesdoctorbalmis.com>
2020-11-18 12:14:30 +0100

    Agregar archivos de proyecto.

---
+++ Actividad4_Calculadora/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Actividad4_Calculadora
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            numerostextBlock.Text +=(sender as Button).Tag.ToString();
+        }
+    }
+}
